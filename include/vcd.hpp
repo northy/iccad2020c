cbeca7d869db96d76bb3eeb6f86f83b7f9b491b9
Alexsandro Thomas <8811216+northy@users.noreply.github.com>
2020-05-10 15:29:32 -0300

    Initial commit + VCD Parser

---
+++ include/vcd.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vector>
+
+namespace vcd {
+    struct Signal {
+        std::string type;
+        int bitwidth;
+        std::string id;
+        std::string name;
+    };
+
+    struct Dump {
+        std::vector<char> value;
+        std::string id;
+    };
+
+    struct Timestamp {
+        uint64_t time;
+        std::vector<Dump> dumps;
+    };
+
+    struct Vcd {
+        std::string date;
+        std::string version;
+        std::optional<std::string> comment;
+        std::string timescale;
+        std::vector<std::string> scope;
+        std::vector<Signal> signals;
+        std::vector<Dump> initial_dump;
+        std::vector<Timestamp> timestamps;
+    };
+}
