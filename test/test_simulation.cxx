2331b49ca4772ab5e72bf2ea5ba9ae863af45572
Alexsandro Thomas <8811216+northy@users.noreply.github.com>
2020-08-31 22:01:20 -0300

    Nand VCD + VCD parsing in test simulation

--- test/test_simulation.cxx
+++ test/test_simulation.cxx
@@ -1,18 +1,29 @@
 #include <vector>
-#include <cstdint>
 #include <tuple>
 #include <queue>
 #include <map>
+#include <set>
 #include <utility>
 #include <iostream>
+#include <iomanip>
 #include <functional>
+#include <sstream>
+
+#include <cstdint>
+#include <ctime>
+
+#include <parser.hpp>
+#include <compiler.hpp>
+
+#include <boost/bimap.hpp>
 
 using std::vector;
+using std::set;
 using std::tuple;
+using std::get;
 using std::priority_queue;
 using std::map;
 using std::pair;
-using std::get;
 using std::cout;
 
 /* Data structures definitions */
@@ -40,6 +51,12 @@ struct Signal {
     }
 };
 vector<Signal> signals;
+
+using signame_bmtype = boost::bimap<uint64_t, std::string>;
+using idname_bmtype = boost::bimap<std::string, std::string>;
+
+using generated_stimulus = pair<uint64_t, Logic>; //id, value;
+map<uint64_t,set<generated_stimulus>> generated_stimuli; //time -> generated_stimulus
 /* Data structures definitions */
 
 /* Queue definitions */
@@ -68,7 +85,7 @@ void queue_dispatch(const QueueItem event) {
     signal.value = signal.recalc(atime);
 
     if (old!=signal.value) {
-        cout << "At time " << atime << " signal " << sigid << " became " << signal.value << '\n';
+        generated_stimuli[atime].insert(generated_stimulus(sigid,signal.value));
         for (uint64_t p : signal.procs) {
             queue_add(atime,processes[p],sigid);
         }
@@ -78,37 +95,87 @@ void queue_dispatch(const QueueItem event) {
 
 map<uint64_t,map<uint64_t,Logic>> stimuli; //Time -> Signal -> value
 
-int main() {
-    /* Example runtime definitions */
-    stimuli = {
-        {uint64_t(0), {
-            {uint64_t(0), Logic(1)}
-        }},
-        {uint64_t(15), {
-            {uint64_t(1), Logic(1)}
-        }},
-        {uint64_t(30), {
-            {uint64_t(0), Logic(0)}
-        }}
-    };
+int main(int argc, char** argv) {
+    std::ios::sync_with_stdio(0); std::cout.tie(0); std::cin.tie(0);
 
+    if (argc<2) { 
+        cout << "Usage: ./TestSimulation file.vcd\n";
+        return 1;
+    }
+
+    auto vcd = parser::parsevcd::parse_vcd_file(argv[1]);
+
+    if (!vcd) {
+        cout << "Error reading VCD file\n";
+        return 1;
+    }
+
+    signame_bmtype signame_bimap;
+    idname_bmtype idname_bimap;
+
+    /* Example runtime definitions */
+    // Circuit:: and = a&b; not = !and;
+    
+    //TODO: autogenerate
+    signame_bimap.insert(signame_bmtype::value_type(0,"a"));
+    signame_bimap.insert(signame_bmtype::value_type(1,"b"));
+    signame_bimap.insert(signame_bmtype::value_type(2,"and"));
+    signame_bimap.insert(signame_bmtype::value_type(3,"not"));
+
+    for (const vcd::Signal s : vcd->signals) {
+        idname_bimap.insert(idname_bmtype::value_type(s.id,s.name));
+    }
+    //TODO: autogenerate
+    idname_bimap.insert(idname_bmtype::value_type("%","and"));
+    idname_bimap.insert(idname_bmtype::value_type("&","not"));
+
+    for (const vcd::Timestamp t : vcd->timestamps) {
+        for (const vcd::Dump d : t.dumps) {
+            const uint64_t signal_id = signame_bimap.right.at(idname_bimap.left.at(d.id));
+            Logic &l = stimuli[t.time][signal_id];
+            const char inp = d.value[0];
+            queue.push(QueueItem(t.time,signal_id));
+            switch (inp) {
+                case '0' :
+                    l = Logic(False);
+                    break;
+                case '1' :
+                    l = Logic(True);
+                    break;
+                case 'x' :
+                    l = Logic(X);
+                    break;
+                case 'z' :
+                    l = Logic(Z);
+                    break;
+            }
+        }
+    }
+
+    //TODO: autogenerate
     activation_functors = {
         [] (uint64_t atime, uint64_t signal) {
             return stimuli[atime][signal];
         },
         [] (uint64_t atime, uint64_t signal) {
             return Logic(signals[0].value && signals[1].value);
-        }
+        },
+        [] (uint64_t atime, uint64_t signal) {
+            return Logic(!signals[2].value);
+        },
     };
 
+    //TODO: autogenerate
     signals = {
         {0,False,{0},0},
         {1,False,{0},0},
-        {2,False,{},1}
+        {2,X,{1},1},
+        {3,X,{},2}
     };
 
+    //TODO: autogenerate
     processes = {
-        {
+        { //and = a&b
             0, {2}, {
                 {uint64_t(0), {
                     {uint64_t(2),uint64_t(0)}
@@ -117,19 +184,66 @@ int main() {
                     {uint64_t(2),uint64_t(0)}
                 }}
             }
+        },
+        { //not = !and
+            1, {3}, {
+                {uint64_t(2), {
+                    {uint64_t(3),uint64_t(0)}
+                }}
+            }
         }
     };
-    
-    queue.push(QueueItem(0,0));
-    queue.push(QueueItem(15,1));
-    queue.push(QueueItem(30,0));
     /* Example runtime definitions */
 
+    /* Run */
     while (!queue.empty()) {
         QueueItem event = queue.top();
         queue.pop();
         queue_dispatch(event);
     }
+    /* Run */
+
+    /* Output VCD */
+    vcd::Vcd new_vcd;
+
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%d/%m/%Y %H:%M:%S");
+    new_vcd.date = oss.str();
+    
+    new_vcd.version = "1.0.0";
+
+    new_vcd.timescale = "1ns";
+
+    new_vcd.scope = {"module logic"};
+
+    for (signame_bmtype::right_map::const_iterator iter = signame_bimap.right.begin(), iend = signame_bimap.right.end(); iter!=iend; ++iter) {
+        vcd::Signal s = {"wire",1,idname_bimap.right.at(iter->first),iter->first};
+        new_vcd.signals.push_back(s);
+    }
+
+    //TODO: autogenerate?
+    for (signame_bmtype::right_map::const_iterator iter = signame_bimap.right.begin(), iend = signame_bimap.right.end(); iter!=iend; ++iter) {
+        vcd::Dump d = {{'x'},idname_bimap.right.at(iter->first)};
+        new_vcd.initial_dump.push_back(d);
+    }
+
+    for (map<uint64_t,set<generated_stimulus>>::const_iterator iter = generated_stimuli.begin(), iend = generated_stimuli.end(); iter!=iend; ++iter) {
+        vcd::Timestamp t;
+        t.time = iter->first;
+        for (set<generated_stimulus>::const_iterator setiter = iter->second.begin(), setiend = iter->second.end(); setiter!=setiend; ++setiter) {
+            vcd::Dump d;
+            d.value = {(char)(setiter->second+48)};
+            d.id = idname_bimap.right.at(signame_bimap.left.at(setiter->first));
+            t.dumps.push_back(d);
+        }
+        new_vcd.timestamps.push_back(t);
+    }
+    new_vcd.timestamps.push_back({new_vcd.timestamps[new_vcd.timestamps.size()-1].time+15,{}});
+
+    compiler::compilevcd::compile_vcd_file(new_vcd, cout);
+    /* Output VCD */
 
     return 0;
 }
